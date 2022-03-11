d30918c2e7a1d6ad19b40b304d63bc85b9cdf6ea
arseni <a.m.halilov@inbox.ru>
2022-03-11 15:03:52 +0300

    home_4

--- src/a.halilov/home_4/task_1/src/js/script.js
+++ src/a.halilov/home_4/task_1/src/js/script.js
@@ -1,3 +1,58 @@
-class generate{
-    constructor()
+
+let addColor = document.getElementById("addColor");
+let topLocation = document.getElementById("top");
+let leftLocation = document.getElementById("left");
+
+
+function circle(){
+    
+    let container = document.getElementById("container");
+    let circle = document.createElement('div');
+    container.append(circle);
+    circle.classList.add("circle");
+    circle.style.backgroundColor = addColor.value;
+    circle.style.top = topLocation.value + "px";
+    circle.style.left = leftLocation.value + "px";
+    let count = 0;
+    count++;
+    circle.innerHTML = count;
+
+
+
+    
 }
+
+function quadrate(){
+    let container = document.getElementById("container");
+    let quadrate = document.createElement('div');
+    container.append(quadrate);
+    quadrate.classList.add("quadrate");
+    quadrate.style.backgroundColor = addColor.value;
+    quadrate.style.top = topLocation.value + "px";
+    quadrate.style.left = leftLocation.value + "px";
+}
+
+function triangle(){
+    let container = document.getElementById("container");
+    let triangle = document.createElement('div');
+    container.append(triangle);
+    triangle.classList.add("triangle");
+    triangle.style.borderBottomColor = addColor.value;
+    triangle.style.top = topLocation.value + "px";
+    triangle.style.left = leftLocation.value + "px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
