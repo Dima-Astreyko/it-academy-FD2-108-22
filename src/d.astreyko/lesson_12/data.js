7a0db4ca94d76527474f065a42cd499038735ec6
Dima Astreyko <75358802+Dima-Astreyko@users.noreply.github.com>
2022-03-18 16:36:07 +0300

    Create data.js

---
+++ src/d.astreyko/lesson_12/data.js
@@ -0,0 +1,72 @@
+const AnimalTree = [
+    {
+      animal: 'Животные',
+      children: [
+        {
+          animal: 'Млекопитающие',
+          children: [
+            {
+              animal: 'Коровы',
+              children: [],
+            },
+            {
+              animal: 'Ослы',
+              children: [],
+            },
+            {
+              animal: 'Собаки',
+              children: [],
+            },
+            {
+              animal: 'Тигры',
+              children: [],
+            },
+          ]
+        },
+        {
+          animal: 'Другие',
+          children: [
+            {
+              animal: 'Змеи',
+              children: [],
+            },
+            {
+              animal: 'Птицы',
+              children: [],
+            },
+            {
+              animal: 'Ящерицы',
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      animal: 'Рыбы',
+      children: [
+        {
+          animal: 'Аквариумные',
+          children: [
+            {
+              animal: 'Гуппи',
+              children: [],
+            },
+            {
+              animal: 'Скалярии',
+              children: [],
+            },
+          ],
+        },
+        {
+          animal: 'Морские',
+          children: [
+            {
+              animal: 'Морская форель',
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ]
